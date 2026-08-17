5a0193e73558ba6503acffb2abd0d30f9c7ed8cd
Andres Castaño <134648791+Felipyan19@users.noreply.github.com>
2026-08-17 17:37:15 -0500

    refactor: remove external identity resolution use case

--- src/modules/access/application/use-cases/resolve-access-profile.ts
+++
@@ -1,9 +0,0 @@
-import type { AccessRepository, AccessResolution, LinkIdentityInput } from '../ports/access-repository';
-
-export class ResolveAccessProfile {
-  constructor(private readonly repository: AccessRepository) {}
-
-  execute(input: LinkIdentityInput): Promise<AccessResolution> {
-    return this.repository.resolveIdentity(input);
-  }
-}
